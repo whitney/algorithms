3ff6cc14ba458acd90c0126426eb87aea7e05056
Whitney Zoller <whitney@wzllr.com>
2012-08-09 23:06:48 -0400

    iterative bsearch

--- binary_search.py
+++ binary_search.py
@@ -2,22 +2,18 @@
 
 import sys
 
-def bin_search(collect, target, left, right):
-	#print "--------------"
-	#print ">>> left %s, right %s " % (left, right)
-	
-	if left > right:
-		return None
-
-	# compute "mid-point"
-	mid = ((right - left) / 2) + left
-	#print ">>> mid %s " % mid
-	if collect[mid] == target:
-		return mid
-	elif collect[mid] > target:
-		return bin_search(collect, target, left, mid-1)
-	else:
-		return bin_search(collect, target, mid+1, right)
+def bsearch(collect, target):
+	l = 0
+	r = len(collect)-1
+	while l <= r:
+		m = (l+r)/2
+		if target == collect[m]:
+			return m
+		elif target < collect[m]:
+			r = m-1
+		else:
+			l = m+1
+	return -1
 
 if __name__ == "__main__":
 	l1 = (1, 2, 5, 9, 12, 14, 17)
@@ -28,8 +24,8 @@ if __name__ == "__main__":
 	t5 = 19
 	print l1
 	print "-------------------"
-	print t1, bin_search(l1, t1, 0, len(l1) - 1)
-	print t2, bin_search(l1, t2, 0, len(l1) - 1)
-	print t3, bin_search(l1, t3, 0, len(l1) - 1)
-	print t4, bin_search(l1, t4, 0, len(l1) - 1)
-	print t5, bin_search(l1, t5, 0, len(l1) - 1)
+	print t1, bsearch(l1, t1)
+	print t2, bsearch(l1, t2)
+	print t3, bsearch(l1, t3)
+	print t4, bsearch(l1, t4)
+	print t5, bsearch(l1, t5)
